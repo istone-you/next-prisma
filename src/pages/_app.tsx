0e809b6c841e97e9a1fdbbe549075ac36624aa51
istone-you <istoneyou1205@yahoo.co.jp>
2023-07-17 11:18:47 +0900

    ✨ Add todo list components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,26 @@
 import '@/styles/globals.css'
 
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
+
+const NEXT_PUBLIC_GRAPHQL_ENDPOINT: string | undefined =
+  process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT
+if (!NEXT_PUBLIC_GRAPHQL_ENDPOINT) {
+  throw new Error('NEXT_PUBLIC_GRAPHQL_ENDPOINT is undefined')
+}
+
+export const client = new ApolloClient({
+  uri: NEXT_PUBLIC_GRAPHQL_ENDPOINT,
+  cache: new InMemoryCache(),
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
     </SessionProvider>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,26 @@
 import { signIn, signOut, useSession } from 'next-auth/react'
 
+import { TodoList } from '@/components/TodoList'
+
 export default function Component() {
   const { data: session } = useSession()
   if (session) {
     return (
-      <>
+      <div className="container mx-auto p-4">
         Signed in as {session.user?.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
+        <button className="p-3 bg-gray-300" onClick={() => signOut()}>
+          Sign out
+        </button>
+        <TodoList />
+      </div>
     )
   }
   return (
-    <>
+    <div className="container mx-auto p-4">
       Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
+      <button className="p-3 bg-gray-300" onClick={() => signIn()}>
+        Sign in
+      </button>
+    </div>
   )
 }
